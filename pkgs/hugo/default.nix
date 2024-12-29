4cbef9bf686796e6ad799e1beb60529de0ad7fa8
fwc <1643806656@qq.com>
2024-12-29 23:26:04 +0800

    feat(hugo): optimize input

--- pkgs/hugo/default.nix
+++ pkgs/hugo/default.nix
@@ -1,20 +1,20 @@
-{ stdenv, fetchurl }:
+{ stdenvNoCC, fetchurl}:
 
 let
   pname = "hugo_extended";
   version = "0.140.1";
   src =
-    if stdenv.hostPlatform.isLinux && stdenv.hostPlatform.system == "x86_64-linux" then
+    if stdenvNoCC.hostPlatform.isLinux && stdenvNoCC.hostPlatform.system == "x86_64-linux" then
       fetchurl {
         url = "https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_extended_${version}_linux-amd64.tar.gz";
-        sha256 = "";
+        sha256 = "sha256-fu9u6RQ63Sgn4p6sNw7k45+dLt3cg1Ocfp8/6tfg9y8=";
       }
-    else if stdenv.hostPlatform.isLinux && stdenv.hostPlatform.system == "aarch64-linux" then
+    else if stdenvNoCC.hostPlatform.isLinux && stdenvNoCC.hostPlatform.system == "aarch64-linux" then
       fetchurl {
         url = "https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_extended_${version}_linux-arm64.tar.gz";
         sha256 = "";
       }
-    else if stdenv.hostPlatform.isDarwin then
+    else if stdenvNoCC.hostPlatform.isDarwin then
       fetchurl {
         url = "https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_extended_${version}_darwin-universal.tar.gz";
         sha256 = "sha256-dLfD+QCsywRipJ64XkykhcnFbrftKI4HXRE3vWV51f0=";
@@ -22,7 +22,7 @@ let
     else
       throw "Unsupported system";
 in
-stdenv.mkDerivation rec {
+stdenvNoCC.mkDerivation rec {
   inherit pname version src;
   buildInputs = [
   ];
